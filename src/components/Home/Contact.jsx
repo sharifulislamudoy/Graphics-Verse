93210b1d3233b91e83cffb797f3e39da8559da8e
sharifulislamudoy <surifroton301@gmail.com>
2026-04-03 18:26:53 +0600

    done

--- src/components/Home/Contact.jsx
+++ src/components/Home/Contact.jsx
@@ -54,7 +54,7 @@ const Contact = () => {
         });
 
         try {
-            const response = await fetch('https://grahic-verse-server.vercel.app/api/contact', {
+            const response = await fetch('https://grahic-verse-server.vercel.app/api/mail', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
