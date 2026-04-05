3c59fa36517f5c992977972c25ef7ef88f0490f5
sharifulislamudoy <surifroton301@gmail.com>
2026-04-05 14:13:27 +0600

    done

--- src/components/Home/Hero.jsx
+++ src/components/Home/Hero.jsx
@@ -35,11 +35,11 @@ const Hero = () => {
         className="text-center md:text-left w-full md:w-3/5 lg:w-2/3"
       >
         <h1 className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-[90px] font-semibold text-[#022F2B] leading-tight">
-          Website & Graphic Designer
+          Graphic Designer
         </h1>
 
         <p className="mt-4 sm:mt-5 md:mt-6 text-base sm:text-lg md:text-xl text-gray-500 max-w-2xl mx-auto md:mx-0">
-          I am a Professional Website & Graphic Designer. This website contains
+          I am a Professional Graphic Designer. This Graphic contains
           design works that I have produced over the past few years. Find
           various types of design projects such as logo designs, brochure
           designs, product packaging designs, website designs, and many more.
